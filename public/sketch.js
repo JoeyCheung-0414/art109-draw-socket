84b585dde979689250c919688fea83ddf028e5ca
JoeyCheung-0414 <joeycheung2003@gmail.com>
2025-11-13 22:16:59 -0800

    socket connect update

--- public/sketch.js
+++ public/sketch.js
@@ -6,7 +6,8 @@ function setup() {
     createCanvas(600, 400);
     background(51);
 
-    socket = io.connect('http://localhost:3000');
+    //socket = io.connect('http://localhost:3000');
+    socket = io();
     socket.on('mouse', NewDrawing);
 }
 
