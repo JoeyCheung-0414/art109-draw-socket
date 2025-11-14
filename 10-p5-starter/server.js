9442e90590ad7603c782fabcfdc11b3cf7e33f63
JoeyCheung-0414 <joeycheung2003@gmail.com>
2025-11-13 22:13:51 -0800

    starting

---
+++ 10-p5-starter/server.js
@@ -0,0 +1,29 @@
+
+var express = require ('express');
+
+var app = express ();
+var server= app.listen(3000);
+
+app.use(express.static('public'));
+//"static" = host unmoving element
+
+console.log ("Server Running");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log ('new connection: ' + socket.id);
+
+    socket.on('mouse', mouseMsg);
+
+    function mouseMsg(data) { 
+        socket.broadcast.emit('mouse', data);
+        //io.sockets.emit('mouse', data); 
+        //^ sends msg to all including original? 
+        console.log(data);
+    }
+}
